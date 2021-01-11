137ce0f881d1c915b5cb1638d8b313b922bdfbe0
anclist <anclist@gmail.com>
2021-01-11 08:21:05 -0500

    Only calculate average score for airlines that have at least one review

--- app/models/airline.rb
+++ app/models/airline.rb
@@ -7,6 +7,7 @@ class Airline < ApplicationRecord
     self.slug = name.parameterize
   end
   def avg_score
+    return 0 unless reviews.count.positive?
     reviews.average(:score).round(2).to_f
   end
 end
